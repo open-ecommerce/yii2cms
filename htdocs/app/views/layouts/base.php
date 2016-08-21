4c44a2ab953670f287f653610a8fe8d552d8fca7
eduardo <eduardo@open-ecommerce.org>
2016-08-21 14:23:54 +0100

    favicons

--- htdocs/app/views/layouts/base.php
+++ htdocs/app/views/layouts/base.php
@@ -11,8 +11,17 @@ $asset = \app\assets\AppAsset::register($this);
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-        <link rel="shortcut icon" href="<?= $asset->baseUrl ?>/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="<?= $asset->baseUrl ?>/favicon.ico" type="image/x-icon">
+
+        <link rel="shortcut icon" href="<?= $asset->baseUrl ?>/favicon/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= $asset->baseUrl ?>/favicon/apple-touch-icon-152x152.png" />
+
         <?php $this->head() ?>
     </head>
     <body>
